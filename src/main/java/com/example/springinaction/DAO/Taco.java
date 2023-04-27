f0417f746e74ca514d6cfc38637d5523f6b2b21b
Sergey <ketxavel@gmail.com>
2023-04-27 14:12:41 +0300

    methods for searching ingredients by Id, OrderController

--- src/main/java/com/example/springinaction/DAO/Taco.java
+++ src/main/java/com/example/springinaction/DAO/Taco.java
@@ -1,11 +1,17 @@
 package com.example.springinaction.DAO;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class Taco {
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
+    @NotNull
+    @Size(min= 1, message = " You need to choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 }
--- src/main/java/com/example/springinaction/controller/DesignTacoController.java
+++ src/main/java/com/example/springinaction/controller/DesignTacoController.java
@@ -5,10 +5,12 @@ import com.example.springinaction.DAO.Ingredient.Type;
 
 import com.example.springinaction.DAO.Taco;
 import com.example.springinaction.DAO.TacoOrder;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -57,9 +59,13 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+        if(errors.hasErrors()){
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
+        
         return "redirect:/orders/current";
     }
 
